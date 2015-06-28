a6e9ebf774b1bd840e612124a42089f0701c9002
ChouAndy <chouandy@ecoworkinc.com>
2015-06-28 17:23:28 +0800

    Fixed .gitignore bugs.

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>Welcome to Apache!</title>
+    <style>
+      body {
+          width: 600px;
+          margin: 30px auto;
+          font-family: Tahoma, Verdana, Arial, sans-serif;
+      }
+      table {
+        word-break: break-all;
+      }
+      table .e {
+        width: 200px;
+      }
+    </style>
+  </head>
+  <body>
+  <div>
+    <h1>Hello world!</h1>
+    <p>If you see this page, the Apache web server is successfully installed and
+    working. Further configuration is required.</p>
+
+    <p><em>Thank you for using Apache.</em></p>
+  </div>
+
+  <?php phpinfo(); ?>
+
+  </body>
+</html>
